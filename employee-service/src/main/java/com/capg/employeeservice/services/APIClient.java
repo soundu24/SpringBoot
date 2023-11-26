bd43b0a9542165072eb9d5206df80b68e7de34fd
Soundarya <soundaryashebbar23@gmail.com>
2023-11-26 11:32:39 +0530

    implemented config server

--- employee-service/src/main/java/com/capg/employeeservice/services/APIClient.java
+++ employee-service/src/main/java/com/capg/employeeservice/services/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.capg.employeeservice.dto.DepartmentDto;
 
 
-@FeignClient(url = "http://localhost:8080/", name = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
 	@GetMapping("api/department/{departmentCode}")
